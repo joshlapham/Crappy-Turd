a40ff3ab063f28c9f83b80c13a91b62c1ea73998
Josh Lapham <josh@joshlapham.com>
2018-08-21 16:36:27 +1000

    Update Game VC as per tutorial

--- Crappy Turd/GameViewController.swift
+++ Crappy Turd/GameViewController.swift
@@ -19,17 +19,15 @@ class GameViewController: UIViewController {
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .resizeFill
                 
                 // Present the scene
                 view.presentScene(scene)
             }
             
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.ignoresSiblingOrder = false
+            view.showsFPS = false
+            view.showsNodeCount = false
         }
     }
     
