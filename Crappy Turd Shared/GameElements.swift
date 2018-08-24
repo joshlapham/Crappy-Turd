ce56d61d59b6d8f3923e65b508e74a7cb7cd690f
Josh Lapham <josh@joshlapham.com>
2018-08-24 17:05:36 +1000

    Misc cleanup

--- Crappy Turd Shared/GameElements.swift
+++ Crappy Turd Shared/GameElements.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2018 Josh Lapham. All rights reserved.
 //
 
-import SpriteKit
-
 struct CollisionBitMask {
     static let turdCategory: UInt32 = 0x1 << 0
     static let pillarCategory: UInt32 = 0x1 << 1
